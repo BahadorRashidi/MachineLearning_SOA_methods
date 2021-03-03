05545ed9b2fe93ed74c6e2a950b23a9c3c4dba49
Bahador <bahador@rtnest.ca>
2021-03-03 10:24:05 -0700

    fininshed simple torch DNN

--- Self Tuning Regression - Bayesian Search/simple_torch_DNN.py
+++ Self Tuning Regression - Bayesian Search/simple_torch_DNN.py
@@ -45,22 +45,16 @@ class simple_torch_DNN(nn.Module):
         '''
         super(simple_torch_DNN, self).__init__()
 
-        self.input_dim, self.output_dim, self.num_hidden, self.hidden_dim, self.activate_func = None, None, None, None, None
         self.activate_func_list = {'ReLU': nn.ReLU(), 'Tanh': nn.Tanh(), 'Sigmoid': nn.Sigmoid()}#we can add more activate function here
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
         self.x_train = torch.FloatTensor(x_train).to(self.device)
         self.y_train = torch.FloatTensor(y_train).to(self.device)
         self.x_val = torch.FloatTensor(x_val).to(self.device)
         self.y_val = torch.FloatTensor(y_val).to(self.device)
-        self.batch_size = None
-        self.func1 = None
-        self.out = None
-        self.hiddens = None
-        self.path = path
         self.parameterization = parameters
 
     def forward(self, X):
-        X = self.func1(X)
+        X = self.fc1(X)
         if not self.hiddens:
             X = self.hiddens(X)
         X = self.out(X)
@@ -82,8 +76,8 @@ class simple_torch_DNN(nn.Module):
 
 
         try: 
-            num_hidden_layers = len(self.parameterization['DNN_hidden_layer_Structure'])
-            DNN_hidden_layer_Structure = self.parameterization['DNN_hidden_layer_Structure']
+            num_hidden_layers = len(self.parameterization['DNN_hidden_layer_Structure']) ## Check the number of hidden layers
+            DNN_hidden_layer_Structure = self.parameterization.get('DNN_hidden_layer_Structure', [32,64,32])
             ## Here we determine the structure of the DNN model
 
             if num_hidden_layers == 1:    
@@ -102,10 +96,12 @@ class simple_torch_DNN(nn.Module):
                 self.hiddens = nn.Sequential(*Layers)
                 model = nn.Sequential(self.fc1, self.hiddens, self.out)
                 return model
+            else:
+                raise Exception('The structure that you provided is not correct!')
         except:
-            print('DNN structure is not provided and a default layer is considered!')
-            self.fc1 = nn.Sequential(nn.Linear(in_features=self.input_dim, out_features=self.hidden_dim), self.activate_func)
-            self.out = nn.Linear(self.hidden_dim, self.output_dim)
+            print('DNN structure is not provided and one hidden layer with 32 neurons is considered!')
+            self.fc1 = nn.Sequential(nn.Linear(in_features=self.input_dim, out_features=32), self.activate_func)
+            self.out = nn.Linear(32, self.output_dim)
             model = nn.Sequential(self.fc1, self.out)
             return model
 
@@ -123,20 +119,25 @@ class simple_torch_DNN(nn.Module):
         model = model.to(self.device)
         loss_function = nn.MSELoss()
         optimizer = torch.optim.SGD(model.parameters(), lr=self.parameterization.get('learning_rate',1e-2),
-                                    momentum=parameterization.get('momentum', 0.9))
+                                    momentum=self.parameterization.get('momentum', 0.9))
 
         num_epochs = self.parameterization.get('epochs', 500)
-        for i in range(num_epochs):
+        batch_size = self.parameterization.get('batch_size', 64)
+        loss_show = []
+        for j in range(num_epochs):
             # x, y = self.shuffle_data() ## This will shuffel the training data for each epoch
-            for i in range(self.x_train.shape[0]//self.parameterization.get('batch_size', 64)):
-                y_pred = model(self.x_train[i*self.parameterization.get('batch_size', 64):(i+1)*self.parameterization.get('batch_size', 64)])
-                optimizer.zer_grad()
-                loss = loss_function(y_pred, self.y_train[i*self.parameterization.get('batch_size', 64):(i+1)*self.parameterization.get('batch_size', 64)])
+            for i in range(self.x_train.shape[0]//batch_size - 1):
+                optimizer.zero_grad()
+                y_pred = model(self.x_train[i*batch_size: (i+1)*batch_size])
+                loss = loss_function(y_pred, self.y_train[i*batch_size: (i+1)*batch_size])
                 loss.backward()
                 optimizer.step()
+            loss_show.append(loss.item())
+            if j%100 == 1:
+                print('epoch: ', j ,'==>loss value:==> ', loss.item())
 
         with torch.no_grad():
-            y_val_pred = model(self.y_val)
+            y_val_pred = model(self.x_val)
             loss_ = loss_function(y_val_pred, self.y_val)
             if np.isnan(loss_.item()):
                 print('Your parameters does not make sense and resulted in Nan!!')
@@ -145,47 +146,76 @@ class simple_torch_DNN(nn.Module):
                 return model, loss_.item()
 
 
-    def fit_model(self, model, visualize = True):
+    def fit_model(self, visualize = True):
         model = self.init_net()
-        model, loss = self.train_net(model)
+        model, loss = self.train_model(model)
 
         if visualize:
             with torch.no_grad():
+                y_pred_train = model(self.x_train)
+                plt.cla()
+                # plt.scatter(x_data, y_data)
+                plt.plot(self.y_train, label='Actual')
+                plt.plot(y_pred_train, color = 'r', lw=3, label='train data predicted')
+                plt.legend()
+                plt.show()
+
                 y_pred = model(self.x_val)
                 y_pred = y_pred.cpu().detach().numpy()
                 x_data = self.x_val.cpu().detach().numpy()
                 y_data = self.y_val.cpu().detach().numpy()
                 plt.cla()
-                plt.scatter(x_data, y_data)
-                plt.plot(x_data, y_pred, color = 'r', lw=3)
+                # plt.scatter(x_data, y_data)
+                plt.plot(y_data, label='Actual')
+                plt.plot(y_pred, color = 'r', lw=3, label='predicted')
+                plt.xlabel('Samples')
+                plt.ylabel('Y')
+                plt.legend()
                 plt.show()
         return loss
 
 
 
+if __name__ == '__main__':
+    parameters = {
+        'DNN_hidden_layer_Structure' : [32,64,32],
+        'activate_fuc' : 'Sigmoid',
+        'learning_rate' : 1e-3,
+        'batch_size': 32,
+        'momentum' : 0.9,
+        'epochs': 500,
+        'activate_fuc': 'ReLU'
+    }
+
+    df =pd.read_csv('data.csv')
+
+    X = df.loc[:,['x1','x2','x3','x4']].values
+    Y = df.loc[:,'Y'].values
+
+    plt.figure()
+    plt.plot(X)
+    plt.xlabel('Samples')
+    plt.ylabel('X')
+    
+    plt.figure()
+    plt.plot(Y)
+    plt.xlabel('Samples')
+    plt.ylabel('Y')
 
 
+    from sklearn.model_selection import train_test_split
+    x_train, x_val, y_train, y_val = train_test_split(X, Y, train_size = 0.65, random_state  = 20, shuffle=True)
 
+    from sklearn.preprocessing import MinMaxScaler
+    X_scale = MinMaxScaler()
+    x_train = X_scale.fit_transform(x_train)
+    x_val = X_scale.transform(x_val)
+    Y_scale = MinMaxScaler()
+    y_train = Y_scale.fit_transform(y_train.reshape(-1,1))
+    y_val = Y_scale.transform(y_val.reshape(-1,1))
 
-        
-        
-
-        
-
-
-
-
-
-
-#%%
-parameters = {
-    'DNN_hidden_layer_Structure' : [32,64,32],
-    'activate_fuc' : 'ReLU',
-
-
-}
-
-
+    defined_class = simple_torch_DNN(x_train, y_train, x_val, y_val, parameters)
+    defined_class.fit_model(visualize = True)
 
 
 
